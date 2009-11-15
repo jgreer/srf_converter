7ef0dcd2fa5f55ea19df232dddb0fc45a9782b94
Justin Greer <github.com@justingreer.com>
2009-11-14 18:02:50 -0600

    Updated to be able to load and animate png images.

--- lib/srf_converter.rb
+++ lib/srf_converter.rb
@@ -1,4 +1,34 @@
 
+class VehicleImage
+  attr_accessor :images_3d, :images_2d
+  def initialize(path)
+    puts "Loading vehicle: #{path}"
+    if path =~ /\.srf$/i
+      @full_image = load_srf(path)
+    else
+      @full_image = javax.imageio.ImageIO.read(java.io.File.new(path))
+    end
+    if @full_image.height >= 300
+      @animated = true
+      raise "Don't yet support animated ones..."
+    else
+      @animated = false
+      @size_3d = (@full_image.width / 360.0).floor * 10
+      @size_2d = ((@full_image.height - @size_3d) / 10.0).floor * 10
+    end
+    puts "  Animated: #{@animated ? 'Yes' : 'No'}"
+    puts "  3D size: #{@size_3d}"
+    puts "  2D size: #{@size_2d}"
+
+    @images_3d = (0..35).to_a.collect { |x| @full_image.get_subimage(x*@size_3d, 0, @size_3d, @size_3d) }
+    @images_2d = (0..35).to_a.collect { |x| @full_image.get_subimage(x*@size_2d, @size_3d, @size_2d, @size_2d) }
+    
+  end
+  def load_srf(path)
+    raise "Can't yet load SRF images."
+  end
+end
+
 class SrfConverter
   def initialize(view_builder)
     @view_builder = view_builder
@@ -16,7 +46,7 @@ class SrfConverter
   
   def load_image(image_file)
     return if image_file.nil?
-    
-    
+    @vehicle = VehicleImage.new(image_file)
+    @view_builder.animation_panel.set_images(@vehicle.images_3d)
   end
 end
--- lib/view_builder.rb
+++ lib/view_builder.rb
@@ -1,5 +1,6 @@
 import javax.swing.JFileChooser
 import javax.swing.filechooser.FileFilter
+import javax.swing.JPanel
 
 class VehicleFileFilter < FileFilter
   def getDescription
@@ -11,9 +12,65 @@ class VehicleFileFilter < FileFilter
   end
 end
 
+class AnimationPanel < JPanel
+  import javax.imageio.ImageIO
+  
+  attr_accessor :interval
+  
+  def initialize(options = {})
+    super()
+    @interval = options[:interval] || 0.12
+    @images = nil
+    @cur_image = nil
+    @cur_spot = nil
+    @thread = nil
+  end
+
+  def paintComponent(graphics)
+    width = getWidth()
+    height = getHeight()
+    bounds = graphics.getClipBounds()
+    if is_background_set
+      graphics.set_color get_background
+      graphics.fillRect(0, 0, width, height)
+    end
+    graphics.draw_image(@cur_image, (width - @cur_image.width) / 2, (height - @cur_image.height) / 2, nil) if @cur_image
+  end
+
+  def set_images(images)
+    @images = images
+    @cur_spot = 0
+    @cur_image = @images[@cur_spot]
+    start
+  end
+  
+  def next_image
+    @cur_spot = (@cur_spot + 1) % @images.length
+    @cur_image = @images[@cur_spot]
+  end
+
+  def start
+    stop if @thread
+    return unless @images
+    @thread = Thread.new { while true; repaint; sleep @interval; next_image; end }
+  end
+
+  def stop
+    return unless @thread
+    @thread.kill
+    @thread = nil
+  end
+
+  def clear
+    stop
+    @images = nil
+    @cur_image = nil
+    repaint
+  end
+end
+
 class ViewBuilder
   import javax.swing.JFrame
-  import javax.swing.JPanel
   import javax.swing.JLabel
   import java.awt.Dimension
   import javax.swing.JMenuBar
@@ -49,7 +106,7 @@ class ViewBuilder
     @frame = JFrame.new('SRF Converter')
     container_panel = JPanel.new
     container_panel.set_layout BoxLayout.new(container_panel, BoxLayout::X_AXIS)
-    @animation_panel = JPanel.new
+    @animation_panel = AnimationPanel.new
     @settings_panel = JPanel.new
     
     @settings_panel.set_background Color.new(0xffcccc)
@@ -80,6 +137,7 @@ class ViewBuilder
     container_panel.add @animation_panel
 
     @frame.size = Dimension.new 400, 300
+    @frame.resizable = false
     @frame.default_close_operation = JFrame::EXIT_ON_CLOSE
 
     @frame
@@ -87,6 +145,6 @@ class ViewBuilder
   
   def get_file_to_open
     result = @file_chooser.show_open_dialog(@frame)
-    (result == JFileChooser::APPROVE_OPTION) ? @file_chooser.get_selected_file.get_name : nil
+    (result == JFileChooser::APPROVE_OPTION) ? @file_chooser.get_selected_file.get_path : nil
   end
 end
